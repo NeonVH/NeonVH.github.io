f0b048c82fa5f5dca233fbd63a0128450f5db0b7
NeonveresVh <gurawshark6245@gmail.com>
2026-03-17 20:27:17 +0700

    Delete constants.js

--- api/constants.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    ERR_CODE: {
-        UNKNOWN: "E000",
-        AUTH_FAIL: "E101",
-        TAMPERED: "E202",
-        FLOOD: "E303"
-    },
-    SYSTEM_NAME: "NEON_CORE_SHIELD"
-};
